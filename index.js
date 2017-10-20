15f17633067788d3c7ca750aa9c859f1b28f5a47
Michael Lekrans <michael.lekrans@chyronhego.com>
2017-10-20 07:16:14 +0200

    fixed bug  in index.js exports .. changed no-this to monkey

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports.rules = {
-  'no-this': require( `${__dirname}/rules/monkey.js` )
+  'monkey': require( `${__dirname}/rules/monkey.js` )
 };
 
 module.exports.configs = {
